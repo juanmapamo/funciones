a23b56156ff916f76a085345f06d549d9cc9f6b6
Laboratorio <juan.patino@udla.edu.ec>
2023-10-23 16:33:26 -0500

    Tercera linea de codigo

--- funciones1.c
+++ funciones1.c
@@ -8,5 +8,11 @@ void main()
 {
     int num1=0, num2=0;
     int resultado;   
-    printf ("Ingrese el 1er valor");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+    printf ("Ingrese el 1er valor");        
+    scanf ("%d", &num1);
+    printf ("Ingrese el 2do valor");  
+    scanf ("%d", &num2);      
+    resultado= sumar (num1, num2);
+    printf ("El resultado es: %d", resultado);
+    // return 1000;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
 } 
