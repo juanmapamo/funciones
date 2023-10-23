53ef441cfd1618557a7ed2c22d860b2978d10d6c
Laboratorio <juan.patino@udla.edu.ec>
2023-10-23 16:27:17 -0500

    Segunda linea de codigo

--- funciones1.c
+++ funciones1.c
@@ -1,1 +1,12 @@
 #include <stdio.h>
+int sumar (int a, int b)
+{
+    int operacion = a + b;
+    return operacion;
+}
+void main() 
+{
+    int num1=0, num2=0;
+    int resultado;   
+    printf ("Ingrese el 1er valor");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+} 
